d801de38cba7baed94c38524dd0c317b9a81c542
Linux User <patricia@kea-dev.members.linode.com>
2021-09-28 15:05:13 +0200

    authentication basics

--- bank_project/bank_app/models.py
+++ bank_project/bank_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
