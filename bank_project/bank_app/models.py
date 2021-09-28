0bc0f1a0112f1ff82813c26d4f74e7f43b69a424
Linux User <vto@kea-dev.members.linode.com>
2021-09-28 12:22:28 +0200

    rank models

--- bank_project/bank_app/models.py
+++ bank_project/bank_app/models.py
@@ -3,5 +3,10 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone = PhoneNumberField()
+    RANK_CHOICES = [('G', 'Gold'), ('S', 'Silver'), ('B', 'Basic')]
+    rank = models.CharField(max_lenght=1, choices=RANK_CHOICES, blank=False)
 
 # Create your models here.
